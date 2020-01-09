ca8ef84283af0fa0a7976e38e24529d212253a97
deng <473189843@qq.com>
2020-01-10 00:18:22 +0800

    完成首页列表问题功能

--- src/main/java/com/community/controller/IndexController.java
+++ src/main/java/com/community/controller/IndexController.java
@@ -1,20 +1,31 @@
 package com.community.controller;
 
+import com.community.dto.QuestionDTO;
+import com.community.mapper.QuestionMapper;
 import com.community.mapper.UserMapper;
+import com.community.model.Question;
 import com.community.model.User;
+import com.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.jws.WebParam;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-        public String index(HttpServletRequest request){
+        public String index(HttpServletRequest request, Model model){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for(Cookie cookie : cookies){
@@ -28,6 +39,10 @@ public class IndexController {
                 }
             }
         }
+
+        List<QuestionDTO> questionsList = questionService.list();
+        model.addAttribute("question",questionsList);
+
         return "index";
-        }
+    }
 }
