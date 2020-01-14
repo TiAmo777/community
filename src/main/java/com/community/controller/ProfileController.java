15e205c3210fd605b9db9ff7e825442558da66dd
deng <473189843@qq.com>
2020-01-15 00:45:34 +0800

    修复分页

--- src/main/java/com/community/controller/ProfileController.java
+++ src/main/java/com/community/controller/ProfileController.java
@@ -1,41 +1,39 @@
 package com.community.controller;
 
 import com.community.dto.PaginationDTO;
-import com.community.dto.QuestionDTO;
-import com.community.mapper.QuestionMapper;
 import com.community.mapper.UserMapper;
-import com.community.model.Question;
 import com.community.model.User;
 import com.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.jws.WebParam;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
-public class IndexController {
+public class ProfileController {
     @Autowired
     private UserMapper userMapper;
-
     @Autowired
     private QuestionService questionService;
 
-    @GetMapping("/")
-    public String index(HttpServletRequest request, Model model,
-                        @RequestParam(name = "page", defaultValue = "1") Integer page,
-                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable(name = "action") String action,
+                          @RequestParam(name = "page", defaultValue = "1") Integer page,
+                          @RequestParam(name = "size", defaultValue = "5") Integer size,
+                          HttpServletRequest request,
+                          Model model){
+        User user = null;
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
+                    user = userMapper.findByToken(token);
                     if (user != null) {
                         request.getSession().setAttribute("user", user);
                     }
@@ -44,8 +42,20 @@ public class IndexController {
             }
         }
 
-        PaginationDTO pagination = questionService.list(page,size);
-        model.addAttribute("pagination", pagination);
-        return "index";
+        if (user == null){
+            return "redirect:/";
+        }
+        if("questions".equals(action)){
+            model.addAttribute("section","questions");
+            model.addAttribute("sectionName","我的提问");
+        }else {
+            model.addAttribute("section","replies");
+            model.addAttribute("sectionName","最新回复");
+        }
+
+        PaginationDTO paginationDTO = questionService.list(user.getId(),page,size);
+
+        model.addAttribute("pagination", paginationDTO);
+        return "profile";
     }
 }
